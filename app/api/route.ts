2eeaf5694cc8f99503395949b5b399183524ac70
micro927 <nsbelieve-15@hotmail.com>
2024-01-11 00:10:52 +0700

    fix: return next response

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,10 @@
+import { NextResponse } from 'next/server';
+
 export async function GET() {
   const res = await fetch(process.env.DATA_JSON_URL as string);
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
-  return res.json();
+
+  return NextResponse.json(res);
 }
