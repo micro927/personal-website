d1a8a27bf269129799f0b32f8c2c54388197efe5
micro927 <nsbelieve-15@hotmail.com>
2024-01-13 21:41:10 +0700

    fix: disable api route for data

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export async function GET() {
-  const res = await fetch(process.env.DATA_JSON_URL as string);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
+// export async function GET() {
+//   const res = await fetch(process.env.DATA_JSON_URL as string);
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data');
+//   }
 
-  return NextResponse.json(res);
-}
+//   return NextResponse.json(res);
+// }
